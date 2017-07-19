a0fae2ff1b9e4334a4cfe846a0cb5b219dcd7546
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2017-07-19 13:56:53 +0500

    simplified via using

--- TestRange.cs
+++ TestRange.cs
@@ -1,3 +1,4 @@
+using System;
 
 class Program
 {
@@ -9,21 +10,21 @@ class Program
 
 
         foreach(int el in range)
-            System.Console.Write(el + " "); // 0 1 2 3 4 5 6 7 8 9
+            Console.Write(el + " "); // 0 1 2 3 4 5 6 7 8 9
 
-        System.Console.WriteLine(range.Sum); // 45
+        Console.WriteLine(range.Sum); // 45
 
         var negRange = new Range(5, -20, -3);
 
-        System.Console.WriteLine(negRange.Count); // 9
+        Console.WriteLine(negRange.Count); // 9
 
-        System.Console.WriteLine( negRange.Contains(-4) ); // True
+        Console.WriteLine( negRange.Contains(-4) ); // True
 
-        System.Console.WriteLine( negRange[5] ); // -10
+        Console.WriteLine( negRange[5] ); // -10
 
         var bigRange = new Range(-567656, 234543234, 27);
 
-        System.Console.WriteLine(bigRange.IndexOf(397378)); // 35742
+        Console.WriteLine(bigRange.IndexOf(397378)); // 35742
 
     }
 
